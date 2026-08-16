18902b57416d69a89de191c575b6c9a19f922111
Mitul <82195661+Chavda-Mitul@users.noreply.github.com>
2026-08-16 11:22:11 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s1, s2) {
+            if (s1.length !== s2.length) return false;
+
+    const count = new Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        count[s1.charCodeAt(i) - 97]++;
+        count[s2.charCodeAt(i) - 97]--;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) return false;
+    }
+
+    return true;
+    }
+}
