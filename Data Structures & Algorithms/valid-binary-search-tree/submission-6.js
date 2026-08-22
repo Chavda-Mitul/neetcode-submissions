1a47f310c2b1496b5175ad94425209e79fab88ed
Mitul <82195661+Chavda-Mitul@users.noreply.github.com>
2026-08-22 11:17:22 +0530

    Add: valid-binary-search-tree - submission-6

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-6.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+
+    helper(root, min=-Infinity, max=Infinity) {
+
+        if(!root){
+            return true;
+        }
+
+        if(root.val <= min || root.val >= max){
+            return false;
+        }
+
+        const left = this.helper(root.left, min, root.val);
+        const right = this.helper(root.right, root.val, max);
+        
+        return left && right;
+    }
+    isValidBST(root) {
+        return this.helper(root);
+    }
+}
